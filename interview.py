d2e6f6c122d6d7c99ee6a42826c8704d76671648
Farkas <nicholasfarkas@rocketcentral.com>
2022-08-22 11:09:35 -0400

    Model Refinement

---
+++ interview.py
@@ -0,0 +1,19 @@
+temp = []
+
+temp.append(1)
+temp.append(3)
+temp.append(7)
+temp.append(4)
+temp.append(2)
+temp.append(8)
+temp.append(9)
+temp.append(4)
+temp.append(2)
+temp.append(4)
+
+print(temp)
+
+x = 12345
+
+
+

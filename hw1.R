9a9994c4774b2dfd6a6b54b07c5d39a66a57e5b0
Farkas <nicholasfarkas@rocketcentral.com>
2022-10-04 18:56:49 -0400

    functions

--- hw1.R
+++ hw1.R
@@ -5,32 +5,37 @@ problem3a <- function(data){
   cat(paste("No of Response Classes: ",length(levels(data$Species))))
 }
 
-problem3a(iris)
-
 problem3b <- function(data){
+  limit <- ncol(data)-1
   print("Mean:")
-  sapply(iris[,1:(ncol(iris)-1)],mean)
+  print(sapply(data[,1:limit],mean))
   print("Median:")
-  sapply(iris[,1:(ncol(iris)-1)],median)
+  print(sapply(data[,1:limit],median))
   print("Mode:")
-  sapply(iris[,1:(ncol(iris)-1)],sd)
+  print(sapply(data[,1:limit],sd))
   print("Minimum:")
-  sapply(iris[,1:(ncol(iris)-1)],min)
+  print(sapply(data[,1:limit],min))
   print("Maximum:")
-  sapply(iris[,1:(ncol(iris)-1)],max)
+  print(sapply(data[,1:limit],max))
 }
 
+problem3c <- function(data){
+  plot(data[,1:2],
+       pch=c("W","L"),
+       col=c("red","blue"),
+       main="Sepal Petal Width vs Sepal Petal Length",
+       ylab="Width (cm)",
+       xlab="Length (cm)",
+  )
+  legend("topright",
+         c("Length","Width"),
+         fill=c("blue","red")
+  )
+}
+
+problem3a(iris)
+problem3b(iris)
+problem3c(iris)
 
 
-plot(iris[,1:2],
-     pch=c("W","L"),
-     col=c("red","blue"),
-     main="Sepal Petal Width vs Sepal Petal Length",
-     ylab="Width",
-     xlab="Length",
-     )
-legend("topright",
-       c("Length","Width"),
-       fill=c("blue","red")
-)
 
